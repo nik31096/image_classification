fe42dcfc6ef132454d8008ce2b5cbbfbee8703a5
Nikita Kostin <n.kostin@partner.samsung.com>
2018-11-21 20:10:26 +0300

    Add softmax_cross_entropy with logits loss to NN

--- tf_mnist.py
+++ tf_mnist.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
 from sklearn.preprocessing import LabelBinarizer
 from matplotlib import pyplot as plt
 from sys import exit
@@ -16,33 +17,57 @@ lb = LabelBinarizer()
 trainY = lb.fit_transform(trainY)
 testY = lb.transform(testY)
 trainX = trainX[:, :, np.newaxis]
-trainY = trainY[:, np.newaxis]
-print(trainX[0].shape)
-print(trainY.shape)
+testX = testX[:, :, np.newaxis]
+trainY = trainY[:, :, np.newaxis]
+testY = testY[:, :, np.newaxis]
 #exit()
+
 # initializers for weights and biases
 w_init = tf.random_normal_initializer(0, 1)
-b_init = tf.constant_initializer(0.1)
+b_init = tf.zeros_initializer()
 
-
-x = tf.placeholder(dtype=tf.float64, shape=[1, data.shape[1]], name='x')
-y = tf.placeholder(dtype=tf.float64, shape=[1, trainY.shape[1], name='y')
+# placeholder for X sample
+x = tf.placeholder(dtype=tf.float64, shape=(trainX.shape[1], None), name='x')
+# placeholder for Y sample
+y = tf.placeholder(dtype=tf.float64, shape=(trainY.shape[1], None), name='y')
 # defining a 2-layer network
-w1 = tf.get_variable(name="w1", shape=[32, data.shape[1]], initializer=w_init, dtype=tf.float64, trainable=True)
-b1 = tf.get_variable(name="b1", shape=[32, 1], initializer=b_init, dtype=tf.float64, trainable=True)
+w1 = tf.get_variable(name="w1", shape=(32, trainX.shape[1]), initializer=w_init, dtype=tf.float64, trainable=True)
+b1 = tf.get_variable(name="b1", shape=(32, 1), initializer=b_init, dtype=tf.float64, trainable=True)
 z1 = tf.nn.relu(tf.matmul(w1, x) + b1)
 
-w2 = tf.get_variable(name="w2", shape=[trainY.shape[1], 32], initializer=w_init, dtype=tf.float64, trainable=True)
-b2 = tf.get_variable(name="b2", shape=[10, 1], initializer=b_init, dtype=tf.float64, trainable=True)
-z2 = tf.nn.sigmoid(tf.matmul(w2, z1) + b2)
+w2 = tf.get_variable(name="w2", shape=(trainY.shape[1], 32), initializer=w_init, dtype=tf.float64, trainable=True)
+b2 = tf.get_variable(name="b2", shape=(10, 1), initializer=b_init, dtype=tf.float64, trainable=True)
+z2 = tf.matmul(w2, z1) + b2
 
-loss = tf.reduce_mean(tf.squared_difference(z2, y))
-optimizer = tf.train.GradientDescentOptimizer().minimize(loss)
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=tf.transpose(z2), labels=tf.transpose(y)), name='loss')
+optimizer = tf.train.GradientDescentOptimizer(0.001).minimize(loss)
 
 with tf.Session() as sess:
     tf.global_variables_initializer().run()
-    for epoch in range(100):
-        for train_x, train_y in zip(trainX, trainY):
-            z2_value, loss_, _ = sess.run(z2, loss, optimizer, feed_dict={x: train_x, y: train_y})
-        
+    train = list(zip(trainX, trainY))
+    for epoch in range(101):
+        losses = []
+        np.random.shuffle(train)
+        for (train_x, train_y) in train[:80]:
+            loss_i, _ = sess.run([loss, optimizer], feed_dict={x: train_x, y: train_y})
+            losses.append(loss_i)
+        epoch_loss = np.mean(losses)
+        if epoch % 10 == 0:
+            print("epoch: {}, loss: {}".format(epoch, epoch_loss))
+
+    preds = []
+    for test_x in testX[:500]:
+        pred = sess.run(tf.nn.softmax(tf.transpose(z2)), feed_dict={x: test_x})
+        preds.append(pred)
+    print(classification_report(testY[:500], preds))
+
+
+
+
+
+
+
+
+
+
 
